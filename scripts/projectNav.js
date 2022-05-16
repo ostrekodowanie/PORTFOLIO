34c52389bf9f5ea83be021d6be73eb68a454b421
Filip <filfer05@gmail.com>
2022-05-16 18:54:31 +0200

    slider

--- scripts/projectNav.js
+++ scripts/projectNav.js
@@ -2,6 +2,9 @@ const navDots = document.querySelectorAll('.dot')
 const slider = document.querySelector('.slider')
 
 const translateImage = () => {
+
+    // translating based on the active dot
+
     let removeActive = () => {
         let activeDot = document.querySelector('.active')
         activeDot.classList.remove('active')
@@ -12,9 +15,10 @@ const translateImage = () => {
             let imageWidth = document.querySelector('.project-images').offsetWidth
             dot.classList.add('active')
             slider.style.transform = `translateX(calc((${imageWidth}px + 3rem)*(-${index})))`
-            console.log(index)
         })
     })
+
+    // automatic translating in every 3 seconds
 
     let counter = 0;
 
@@ -30,6 +34,36 @@ const translateImage = () => {
         }, 3000)
     }
     automatic()
+
+    // swiping event
+
+    
+
+    const setSwipe = () => {
+        let startX = 0;
+        let endX = 0;
+
+        slider.addEventListener('touchstart', (e) => {
+            let touch = e.changedTouches
+            startX = touch[0].screenX
+        })
+
+        slider.addEventListener('touchend', (e) => {
+            let touches = [...e.changedTouches]
+            touches.forEach((touch) => {
+                endX = touch.screenX
+                if (startX > endX) {
+                    removeActive()
+                    counter++;
+                    navDots[counter].classList.add('active')
+                    let imageWidth = document.querySelector('.project-images').offsetWidth
+                    slider.style.transform = `translateX(calc((${imageWidth}px + 3rem)*(-1)))`
+                }
+            })
+            setSwipe()
+        })
+    }
+    setSwipe()
 }
 
 export default translateImage;
